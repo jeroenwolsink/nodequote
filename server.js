4cc9510a5b314abfb8bdf12857adcb136843965c
jwolsink <jeroen@jwse.nl>
2018-05-02 22:00:55 +0200

    Added docker support and eslint

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 const fs = require('fs');
-const csv = require("fast-csv")
+const csv = require('fast-csv');
 
-var express = require('express'),
-  app = express(),
-  port = process.env.PORT || 3000;
-var router = express.Router();
-var quotes = [];
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
+const router = new express.Router();
+const quotes = [];
 
 router.get('/', function(req, res) {
-    res.json({ message: 'Node quote initialized with ' + quotes.length + ' quotes.' });
+    res.json({message: 'Node quote initialized with '
+    + quotes.length + ' quotes.'});
 });
 
-router.get('/quote', function(req, res) {    
-    var randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+router.get('/quote', function(req, res) {
+    let randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
     res.json(randomQuote);
 });
 
@@ -20,15 +21,15 @@ app.use('/api', router);
 
 app.listen(port);
 
-var stream = fs.createReadStream("quotes.csv");
+let stream = fs.createReadStream('quotes.csv');
 
 csv
- .fromStream(stream, {headers : ["id", "content", "author"]})
- .on("data", function(data){
-    quotes.push(data); 
- })
- .on("end", function(){
-     console.log("Finished loading quotes from file");
- });
+    .fromStream(stream, {headers: ['id', 'content', 'author']})
+    .on('data', function(data) {
+        quotes.push(data);
+    })
+    .on('end', function() {
+        console.log('Finished loading quotes from file');
+    });
 
 console.log('Node Quote RESTful API server started on: ' + port);
