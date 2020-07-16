a64746679cd1ae6a5148348ed0d39f514ab28d5f
Jeroen Wolsink <jeroen.wolsink@rotra.nl>
2020-07-16 08:35:27 +0200

    Update dependencies

--- server.js
+++ server.js
@@ -1,36 +1,34 @@
-const fs = require('fs');
-const csv = require('fast-csv');
+const fs = require("fs");
+const csv = require("fast-csv");
 
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 8080;
 const router = new express.Router();
 const quotes = [];
 
-router.get('/', function(req, res) {
-    res.json({
-        message: 'Node quote initialized with ' + quotes.length + ' quotes.',
-    });
+router.get("/", function (req, res) {
+  res.json({
+    message: "Node quote initialized with " + quotes.length + " quotes.",
+  });
 });
 
-router.get('/quote', function(req, res) {
-    let randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
-    res.json(randomQuote);
+router.get("/quote", function (req, res) {
+  let randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+  res.json(randomQuote);
 });
 
-app.use('/api', router);
+app.use("/api", router);
 
 app.listen(port);
 
-let stream = fs.createReadStream('quotes.csv');
+fs.createReadStream("quotes.csv")
+  .pipe(csv.parse({ headers: ["id", "content", "author"] }))
+  .on("data", function (data) {
+    quotes.push(data);
+  })
+  .on("end", function () {
+    console.log("Finished loading quotes from file");
+  });
 
-csv
-    .fromStream(stream, {headers: ['id', 'content', 'author']})
-    .on('data', function(data) {
-        quotes.push(data);
-    })
-    .on('end', function() {
-        console.log('Finished loading quotes from file');
-    });
-
-console.log('Node Quote RESTful API server started on: ' + port);
+console.log("Node Quote RESTful API server started on: " + port);
